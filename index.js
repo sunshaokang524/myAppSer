68193deafa0bed5f83cde86e691f822ae45bcbfe
sunshaokang <9240603+aidongh@user.noreply.gitee.com>
2024-02-05 08:33:48 +0800

    注册接口

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const url = "mongodb://localhost:27017/myApp";
 const cors = require('cors')
+const uuid = require('node-uuid');
+const CryptoJS = require("crypto-js");
 api.use(cors())
+
+function encrypt(key,text){
+  return CryptoJS.AES.encrypt(text,key).toString();
+}
 mongoose.connect(url);
 mongoose.connection.on("connected", function () {
     console.log("连接成功：", url);
   });
   let userSchema = new mongoose.Schema({
-    account: String,
-    userName: String,
+    phone: String,
     password:String,
     id:String
   });
@@ -24,11 +29,13 @@ mongoose.connection.on("connected", function () {
   api.listen(3000, "192.168.0.2", () => {
     console.log("服务器启动");
   });
-  // User.find().then((result) => {
-  //   console.log(result,'445')
-  //   });
+ 
+
     // 注册
     api.post("/signIn", (req, res) => {
-      console.log(req.body)
-      res.send("请求成功！");
+      const uuid1 = uuid.v1();
+      req.body.passWord=encrypt('wodeappjiushihao',req.body.passWord)
+      let u = new User({id:uuid1,phone:req.body.userPhone,password:req.body.passWord});
+      u.save();
+      res.send({data:{},code:200,message:'注册成功！'});
     });
